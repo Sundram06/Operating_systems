a8248c57ee32773a161663bfebe243583a52c5f2
Sundram06 <46992304+Sundram06@users.noreply.github.com>
2019-04-19 13:14:42 +0530

    Update priority.cpp

--- priority.cpp
+++ priority.cpp
@@ -1,71 +1,99 @@
-#include<iostream>
- 
-using namespace std;
- 
-int main()
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NonPreemptivePriorityScheduling 
 {
-    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
-    cout<<"Enter Total Number of Process:";
-    cin>>n;
- 
-    cout<<"\nEnter Burst Time and Priority\n";
-    for(i=0;i<n;i++)
-    {
-        cout<<"\nP["<<i+1<<"]\n";
-        cout<<"Burst Time:";
-        cin>>bt[i];
-        cout<<"Priority:";
-        cin>>pr[i];
-        p[i]=i+1;           
-    }
- 
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(pr[j]<pr[pos])
-                pos=j;
+    public static Scanner sc = new Scanner(System.in);
+    public static ArrayList<SProcess> p = new ArrayList<>();
+    public static void addProcess(SProcess obj) {
+        int j;
+        int s = p.size();
+        boolean bool = false;
+        obj.number = s + 1;
+        obj.statue = 0;
+        p.add(obj);
+        p.sort(null);
+        for (j = 0; j < s; j++) {
+            if (p.get(j).priority > obj.priority) {
+                bool = true;
+                p.add(j, obj);
+                break;
+            }
         }
- 
-        temp=pr[i];
-        pr[i]=pr[pos];
-        pr[pos]=temp;
- 
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
- 
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
+        if (bool != true) {
+            p.add(s, obj);
+        }
     }
- 
-    wt[0]=0;            
- 
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
- 
-        total+=wt[i];
+
+    public static void gamal() {
+        int i, j;
+        int number = p.size();
+        int location = 0;
+        int minimum = p.get(0).arrivalTime;
+        for (i = 1; i < number; i++) {
+            p.get(i).statue = 0;
+            if (p.get(i).arrivalTime < minimum) {
+                minimum = p.get(i).arrivalTime;
+                location = i;
+            }
+        }
+        p.get(location).waitTime = 0;
+        p.get(location).turnaroundTime = p.get(location).brustTime;
+        p.get(location).responseTime = 0;
+        p.get(location).statue = 1;
+        int totalbrusttime = p.get(location).turnaroundTime;
+        while (true) {
+            boolean pool = true;
+            for (i = 0; i < number; i++) {
+                while (p.get(i).arrivalTime <= totalbrusttime && p.get(i).statue == 0) {
+                    p.get(i).waitTime = p.get(location).turnaroundTime - p.get(i).arrivalTime;
+                    p.get(i).waitTime = p.get(i).waitTime < 0 ? 0 : p.get(i).waitTime;
+
+                    if (p.get(i).waitTime == 0) {
+                        p.get(i).turnaroundTime = p.get(i).brustTime + p.get(i).arrivalTime;
+                    } else {
+                        p.get(i).turnaroundTime = p.get(location).turnaroundTime + p.get(i).brustTime;
+                    }
+                    p.get(i).turnaroundTime = p.get(location).turnaroundTime + p.get(i).brustTime;
+                    p.get(i).responseTime = p.get(i).waitTime;
+                    p.get(i).statue = 1;
+                    //System.out.println(p.get(i).number);
+                    //System.out.println(p.get(i).waitTime);
+                    //System.out.println(p.get(i).turnaroundTime);
+                    location = i;
+                    pool = false;
+                    totalbrusttime = p.get(i).turnaroundTime;
+                    break;
+                }
+            }
+            if (pool == true) {
+                boolean done = true;
+                for (int l = 0; l < p.size(); l++) {
+                    if (p.get(l).statue == 0) {
+                        done = false;
+                        break;
+                    }
+                }
+                if (done) {
+                    break;
+                }
+                j = 0;
+                for (int t = 0; t < number; t++) {
+                    if (p.get(t).statue == 0) {
+                        minimum = p.get(t).arrivalTime;
+                        totalbrusttime = minimum;
+                        break;
+                    }
+                }
+                while (j < number) {
+                    if (p.get(j).statue == 0 && p.get(j).arrivalTime < minimum) {
+                        minimum = p.get(j).arrivalTime;
+                        totalbrusttime = minimum;
+                    }
+                    j++;
+                }
+            }
+        }
+        System.out.println(p);
     }
- 
-    avg_wt=total/n;     
-    total=0;
- 
-    cout<<"\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time";
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];     
-        total+=tat[i];
-        cout<<"\nP["<<p[i]<<"]\t\t  "<<bt[i]<<"\t\t    "<<wt[i]<<"\t\t\t"<<tat[i];
-    }
- 
-    avg_tat=total/n;     
-    cout<<"\n\nAverage Waiting Time="<<avg_wt;
-    cout<<"\nAverage Turnaround Time="<<avg_tat;
- 
-    return 0;
 }
